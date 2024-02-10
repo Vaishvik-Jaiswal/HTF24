564e4715bc0decde715dff3452ec450540ff1067
abolipatne01 <abolipatne01@gmail.com>
2024-02-10 13:23:17 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './landingPage'; 
+import Login from './login';
 
 function App() {
   return (
-    <LandingPage /> 
+    // <LandingPage />
+    <Login /> 
   );
 }
 
