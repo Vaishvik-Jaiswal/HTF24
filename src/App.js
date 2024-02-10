a304f419dd6b06ddd49afad3d8a8dc0d57bfbd94
apadia25 <akshaypadia25@gmail.com>
2024-02-10 13:16:00 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './landingPage'; 
+import Patient1 from './Patient1';
 
 function App() {
   return (
-    <LandingPage /> 
+    // <LandingPage /> 
+    <Patient1 />
   );
 }
 
